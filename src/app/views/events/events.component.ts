90727ad8f30eaf831aa36db68b16141833375de8
Vorozhev Evgenii <nef.form@mail.ru>
2023-01-24 16:23:12 +0500

    add json placemark and event widget vk

--- src/app/views/events/events.component.ts
+++ src/app/views/events/events.component.ts
@@ -1,5 +1,6 @@
 import { EventsService } from '../../services/events.service';
 import { IEvents } from '../../models/events';
+import { async } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,14 +9,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
-  events: IEvents[]=[]
+
+  eventPost!:string
+   loadAPI!: Promise<any>;
+  events: IEvents[]=[
+    {
+      "vkPost":"VK.Widgets.Post('vk_post_-39122624_142882', -39122624, 142882, 'BfmvMagoDLy0PVNWg0RDk-G9t1cH');",
+      "vkIdPost":"vk_post_-39122624_142882",
+    },
+    {
+      "vkPost":"VK.Widgets.Post('vk_post_1_45616', 1, 45616, '4mR-o2_COU6XjdBDA9Afjr8qcN7J');",
+      "vkIdPost":"vk_post_1_45616",
+    },
+    {
+      "vkPost":"VK.Widgets.Post('vk_post_-39122624_142855', -39122624, 142855, 'lFWNGE8x7FoHY5TxFxjdkfGhMjQ9');",
+      "vkIdPost":"vk_post_-39122624_142855",
+    },
+  ]
 
   constructor(public eventsService: EventsService) { }
 
   ngOnInit() {
-    this.eventsService.getAll().subscribe(() => {
-      // this.loading = false
+    // this.eventsService.getAll().subscribe(() => {
+    //   // this.loading = false
+    // })
+
+// console.log(this.eventsService)
+    this.events.forEach(async element  => {
+      // console.log(element)
+        this.loadAPI = new Promise(async (resolve) => {
+        console.log('resolving promise...');
+        await this.loadScript(element.vkPost);
+        // await this.loadScript("VK.init({apiId: 51529720, onlyWidgets: true});");
+        // await this.loadScript('VK.Widgets.Comments("vk_comments", {limit: 15, attach: "*"});');
+        this.eventPost=element.vkIdPost;
+        
+    });
     })
+
+
+
+    // this.eventsService.getAll().subscribe(() => {
+    // })
+  }
+
+//Чтение скриптов
+  public loadScript(vkPost:string) {
+    console.log('preparing to load...')
+    let node = document.createElement('script');
+    // node.src = "./script.js";
+    node.text=vkPost
+    node.type = 'text/javascript';
+    node.async = true;
+    node.charset = 'utf-8';
+    document.getElementsByTagName('head')[0].appendChild(node);
   }
 
 }
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -35,10 +35,21 @@ export class HomeComponent implements OnInit {
       "vkIdPost":"vk_post_1_45616",
       "preset": "islands#greenDotIcon",
       "iconColor": "#735184",
+      "image":"https://sun3.userapi.com/sun3-11/s/v1/ig2/QtC5BaL_C8PDOTfazM51SI6b4jhx-EYA8zlbg6f-G5vSsGtXuqMRi7l9fgnYyWhY8HLvcezF6Zw74FPLiVqJkfy8.jpg?size=604x376&quality=95&type=album",
       "properties": {
         "balloonContentHeader":"Заголовок если необходим",
-        "balloonContentBody":"Многие из нас слышали о квантовых компьютерах, но знают о том, что они из себя представляю – далеко не все. Давайте разбираться.",
-        "balloonContentFooter":"footer",
+        "balloonContentBody":`🚉Ориентация – север: с сегодняшнего дня стартуют рейсы "Ласточки" в северные города Свердловской области 🌠
+
+        <br>📆С 20 января Свердловская железная дорога запускает ежедневные рейсы "Ласточек" по маршруту Екатеринбург – Серов – Бокситы (город Североуральск). Время в пути – 7,5 часов.
+        <br>📌Остановки будут на следующих станциях: ВИЗ, Нижний Тагил, Смычка, Баранчинская, Гороблагодатская, Кушва, Верхняя, Выя, Верхотурье, Ляля, Лобва, Серов, Краснотурьинская, Лесная Волчанка.
+        <br>📜Расписание рейсов – следующее👇:
+        <br>⏱Поезд №7076: от станции Бокситы – отправление в 4:15, в Серов состав прибудет в 6:02, а в Екатеринбург – в 11:44.
+        <br>⏱Обратный поезд № 7075: из Екатеринбурга отправление в 16:00, из Серова – в 21:56 и прибытие в Североуральск – в 23:30.
+        <br>✅Напомним: раньше добраться до станции Бокситы из Екатеринбурга можно было только пересев в Серове на междугородний автобус. Запуск новых поездов позволил сократить этот путь на 2 часа.
+        <br>💰До конца января стоимость билета от Екатеринбурга до Боксит составит 883 рубля. Будут действовать все виды льгот, установленных для проезда в пригородном железнодорожном транспорте.
+        
+        <br>По информации "ОГ"<br> <img src="https://sun3.userapi.com/sun3-11/s/v1/ig2/QtC5BaL_C8PDOTfazM51SI6b4jhx-EYA8zlbg6f-G5vSsGtXuqMRi7l9fgnYyWhY8HLvcezF6Zw74FPLiVqJkfy8.jpg?size=604x376&quality=95&type=album">`,
+        "balloonContentFooter":'<img src="https://sun3.userapi.com/sun3-11/s/v1/ig2/QtC5BaL_C8PDOTfazM51SI6b4jhx-EYA8zlbg6f-G5vSsGtXuqMRi7l9fgnYyWhY8HLvcezF6Zw74FPLiVqJkfy8.jpg?size=604x376&quality=95&type=album">',
         "hintContent":"Содержание всплывающей подсказки",
       },
       "options":{
@@ -54,7 +65,7 @@ export class HomeComponent implements OnInit {
       "iconColor": "#735184",
       "properties": {
         "balloonContentHeader":"Тропические циклоны, торнадо (зарождение, причины устойчивости).",
-        "balloonContentBody":"<img url='http://psy.tom.ru/pic/tornado_01.gif'/><br>Статья о природе атмосферных стихий. В частности, рассматривается процесс конденсации переохлаждённого водяного пара, который может вызвать резкое понижение давления, шквалистые ветры и вихри. Вероятно, образующаяся воронка вихря раскручивается ускоренными воздушными потоками, огибающими турбулентные «выступы»",
+        "balloonContentBody":'В последние месяцы все более модной становится тема эмиграции из России. По обыкновению, пойду против тренда — публикую 7 причин оставаться в России.<br><div class="page_post_sized_thumbs  clear_fix" style="width: 208px; height: 129px; "><img style="background: #FFF url(/images/progress7.gif) center no-repeat" src="https://sun3.userapi.com/sun3-11/s/v1/if1/HHTFuaEJ0DzNRgGAqyaeep4zpSepS-I1b0EHY2YboqKlmF44oIUJjpaMSFs-cOjoecOsDA.jpg?size=604x539&amp;quality=96&amp;type=album"><br><img style="background: #FFF url(/images/progress7.gif) center no-repeat" src="https://sun3.userapi.com/sun3-8/s/v1/if1/6G1GvFidWlHTGjv2vVD6743YK7zvvKuUQldGgjXHwAxqS22AvHyJt8rjh-FQTaTkl2Zaeg.jpg?size=604x539&amp;quality=96&amp;type=album"></div>',
         "balloonContentFooter":"footer",
         "hintContent":"Содержание всплывающей подсказки",
       },
@@ -70,9 +81,8 @@ export class HomeComponent implements OnInit {
       "preset": "islands#greenDotIcon",
       "iconColor": "#735184",
       "properties": {
-        "balloonContentHeader":"Заголовок если необходим",
-        "balloonContentBody":"<div id='vk_post_1_45616'></div>  $('body').append(menuContent);",
-        "balloonContentLayout":"<div id='vk_post_1_45616'></div> ",
+        "balloonContentHeader":"Тропические циклоны, торнадо (зарождение, причины устойчивости).",
+        "balloonContentBody":'В последние месяцы все более модной становится тема эмиграции из России. По обыкновению, пойду против тренда — публикую 7 причин оставаться в России.<br><div class="page_post_sized_thumbs  clear_fix" style="width: 208px; height: 129px; "><img style="background: #FFF url(/images/progress7.gif) center no-repeat" src="https://sun3.userapi.com/sun3-11/s/v1/if1/HHTFuaEJ0DzNRgGAqyaeep4zpSepS-I1b0EHY2YboqKlmF44oIUJjpaMSFs-cOjoecOsDA.jpg?size=604x539&amp;quality=96&amp;type=album"><br><img style="background: #FFF url(/images/progress7.gif) center no-repeat" src="https://sun3.userapi.com/sun3-8/s/v1/if1/6G1GvFidWlHTGjv2vVD6743YK7zvvKuUQldGgjXHwAxqS22AvHyJt8rjh-FQTaTkl2Zaeg.jpg?size=604x539&amp;quality=96&amp;type=album"></div>',
         "balloonContentFooter":"footer",
         "hintContent":"Содержание всплывающей подсказки",
       },
@@ -103,47 +113,49 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.points.forEach(element => {
+   this.points.forEach(element => {
       // console.log(element)
       this.placemarks.push({
         geometry: element.geometry,
         vkPost:element.vkPost,
         vkIdPost:element.vkIdPost,
         properties: {
-        // balloonPanelMaxMapArea: 0,
+         balloonPanelMaxMapArea: 0,
+         balloonContentBody:element.properties.balloonContentBody,
+        //  balloonContentFooter:element.properties.balloonContentFooter,
         // hintContent: element.properties.hintContent,
         },
         options: {
           preset: element.options.preset, //вид иконок, если необходимо будет менять
           iconColor: element.options.iconColor, //цвет иконок, если необходимо будет менять
           openEmptyBalloon: true,
-        },
+    },
       });
-    });
+  });
   }
 
 
 //Чтение скриптов
-public loadScript(vkPost:string) {
-  console.log('preparing to load...')
-  let node = document.createElement('script');
-  // node.src = "./script.js";
-  node.text=vkPost
-  node.type = 'text/javascript';
-  node.async = true;
-  node.charset = 'utf-8';
-  document.getElementsByTagName('head')[0].appendChild(node);
-}
+// public loadScript(vkPost:string) {
+//   console.log('preparing to load...')
+//   let node = document.createElement('script');
+//   // node.src = "./script.js";
+//   node.text=vkPost
+//   node.type = 'text/javascript';
+//   node.async = true;
+//   node.charset = 'utf-8';
+//   document.getElementsByTagName('head')[0].appendChild(node);
+// }
 
 //Вывод поста ВК в метку
-  onPlacemarkBalloonOpen(event: YaEvent<ymaps.Placemark>,vkPost:string, vkIdPost:string): void {
-    const { target } = event;
-    let q=vkPost
-    console.log(target)
-      this.loadAPI = new Promise(async(resolve) => {
-        console.log('resolving promise...');
-        await this.loadScript(vkPost);
-        target.properties.set('balloonContent', `<div style="width:1000rem; height:1000rem"><div id="${vkIdPost}"></div></div>`);
-    });
-  }
+  // onPlacemarkBalloonOpen(event: YaEvent<ymaps.Placemark>,vkPost:string, vkIdPost:string): void {
+  //   const { target } = event;
+  //   let q=vkPost
+  //   console.log(target)
+  //     this.loadAPI = new Promise(async(resolve) => {
+  //       console.log('resolving promise...');
+  //       await this.loadScript(vkPost);
+  //       // target.properties.set('balloonContent', `<div style="width:1000rem; height:1000rem"><div id="${vkIdPost}"></div></div>`);
+  //   });
+  // }
 }
