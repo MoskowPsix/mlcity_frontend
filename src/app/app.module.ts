8f1ffb3093d2e8f5cb05f9e3c1814bbeaea7f1bc
Ruslan Shiryaev <refus91@mail.ru>
2023-01-25 09:21:26 +0500

    add d ClickOutside

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoginComponent } from './views/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AddEventComponent } from './views/add-event/add-event.component';
 
 import { AuthService } from './services/auth.service';
 import { ToastService } from './services/toast.service'; 
@@ -32,7 +33,8 @@ import { XsrfInterceptor } from './xsrf.interceptor';
 
 import { AuthGuard } from './guards/auth.guard';
 import { LoggedInAuthGuard } from './guards/logged-in-auth.guard';
-import { AddEventComponent } from './views/add-event/add-event.component';
+
+import { ClickOutsideDirective } from './directives/click-outside.directive';
 
 const mapConfig: YaConfig = {
   apikey: environment.apiKeyYandex,
@@ -50,6 +52,7 @@ const mapConfig: YaConfig = {
     HeaderComponent,
     SidebarComponent,
     AddEventComponent,
+    ClickOutsideDirective,
   ],
   imports: [
     BrowserModule, 
