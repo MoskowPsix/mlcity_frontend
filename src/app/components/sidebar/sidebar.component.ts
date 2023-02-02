4c11eb74f42a7bec1f0c836590d3e3cbccb60aa4
Ruslan Shiryaev <refus91@mail.ru>
2023-02-02 22:10:42 +0500

    rafactor styles, del Tilewind

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router';
 import menuPublicData from '../../../assets/json/menu-public.json'
 import { IMenu } from 'src/app/models/menu'
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,8 +13,12 @@ import { IMenu } from 'src/app/models/menu'
 export class SidebarComponent implements OnInit {
   menuPublic: IMenu[] = []
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
   
+  onLogout(){
+    this.authService.logout()
+  }
+
   public isLChildLinkActive(): boolean {
     return this.router.isActive('/contacts/support', true) || this.router.isActive('/contacts/feedback', true);
   }
