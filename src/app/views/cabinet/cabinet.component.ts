8947bf4f52076845fc62dda81229252bb5865440
Ruslan Shiryaev <refus91@mail.ru>
2023-02-04 00:27:43 +0500

    modified c Cabinet

--- src/app/views/cabinet/cabinet.component.ts
+++ src/app/views/cabinet/cabinet.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { UserService } from 'src/app/services/user.service';
-import { ToastService } from 'src/app/services/toast.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cabinet',
@@ -9,34 +6,9 @@ import { ToastService } from 'src/app/services/toast.service';
   styleUrls: ['./cabinet.component.scss'],
 })
 export class CabinetComponent implements OnInit {
-  // subscription_1!: Subscription 
-  // subscriptions: Subscription[] = []
-  // user: any
-  // socialAccount: any
   
-  constructor(private userService: UserService, private toastService: ToastService) { }
+  constructor() { }
 
-  ngOnInit() {
-    // this.user = this.userService.getUser()
+  ngOnInit() {}
 
-    //Грузим инфу об оккаунте соц сети
-    // this.subscription_1 = this.userService.getSocialAccountByUserId(this.user.id).subscribe({
-    //   next: data => {
-    //     this.socialAccount = data
-    //   },
-    //   error: err => {
-    //     this.toastService.showToast(err.error.message, 'warning')
-    //   }
-    // })
-
-    //this.socialAccount = this.userService.getSocialAccountByUserId(this.user.id)
-
-    //Добавляем подписки в массив
-    // this.subscriptions.push(this.subscription_1)
-  }
-
-  ngOnDestroy() {
-    // отписываемся от всех подписок
-  //  this.subscriptions.forEach((subscription) => subscription.unsubscribe())
- }
 }
