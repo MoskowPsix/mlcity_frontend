8cef79257bc70d21514c2d050bd2657d89851095
Ruslan Shiryaev <refus91@mail.ru>
2023-03-24 15:21:44 +0500

    edit env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: false,
   apiKeyYandex: '3a8040b4-161a-485b-b16e-1968065fb4ac',
   APP_NAME: 'MyLittleCity',
-  BASE_URL: 'http://mlcity.ru',
+  BASE_URL: 'https://mlcity.ru',
   PORT: '443',
-  BACKEND_URL: 'http://mlcity.ru',
+  BACKEND_URL: 'https://mlcity.ru',
   BACKEND_PORT: '3443',
   // BASE_URL: 'http://localhost',
   // PORT: '8000',
   //vkontakteAuthUrl: `http://localhost:8000/api/social-auth/vkontakte`
-  vkontakteAuthUrl: `http://mlcity.ru:3443/api/social-auth/vkontakte`
+  vkontakteAuthUrl: `https://mlcity.ru:3443/api/social-auth/vkontakte`
 };
