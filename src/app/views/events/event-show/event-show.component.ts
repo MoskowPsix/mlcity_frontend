3d0049a0a5219707d031fd846f9a115900484e9d
Ruslan Shiryaev <refus91@mail.ru>
2023-03-30 12:30:11 +0500

    edit event show

--- src/app/views/events/event-show/event-show.component.ts
+++ src/app/views/events/event-show/event-show.component.ts
@@ -1,23 +1,17 @@
 import { Component, OnInit,OnDestroy, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
-import { retry } from 'rxjs/internal/operators/retry';
-import { tap } from 'rxjs/internal/operators/tap';
+import { Subject, takeUntil, tap, retry, catchError, of, EMPTY} from 'rxjs';
 import { IEvent } from 'src/app/models/events';
 import { EventsService } from 'src/app/services/events.service';
 import { IonicSlides } from '@ionic/angular';
 import {register} from 'swiper/element/bundle';
-import {Swiper} from 'swiper/types';
 import { environment } from 'src/environments/environment';
 import { YaReadyEvent } from 'angular8-yandex-maps';
 import { MessagesAuth } from 'src/app/enums/messages-auth';
 import { ToastService } from 'src/app/services/toast.service';
-import { catchError } from 'rxjs/internal/operators/catchError';
-import { of } from 'rxjs/internal/observable/of';
-import { EMPTY } from 'rxjs/internal/observable/empty';
 import { MessagesErrors } from 'src/app/enums/messages-errors';
 import { AuthService } from 'src/app/services/auth.service';
-import { switchMap } from 'rxjs/internal/operators/switchMap';
+import { Swiper } from 'swiper';
 
 @Component({
   selector: 'app-event-show',
@@ -27,11 +21,11 @@ import { switchMap } from 'rxjs/internal/operators/switchMap';
 export class EventShowComponent implements OnInit, OnDestroy, AfterViewInit {
   private readonly destroy$ = new Subject<void>()
 
-  // @ViewChild('swiper')
-  // swiperRef: ElementRef | undefined;
-  // swiper?: Swiper;
-  // swiperCurrentSlide?: number
-  // swiperTotalSlids?: number
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+  swiperCurrentSlide?: number
+  swiperTotalSlids?: number
 
   swiperModules = [IonicSlides];
 
@@ -68,7 +62,6 @@ export class EventShowComponent implements OnInit, OnDestroy, AfterViewInit {
       if(event)
         this.event = event
         this.startLikesCount = this.event?.likes ? this.event.likes.vk_count + this.event.likes.local_count : 0
-        console.log(this.event)
     }); 
   }
 
@@ -79,7 +72,6 @@ export class EventShowComponent implements OnInit, OnDestroy, AfterViewInit {
         takeUntil(this.destroy$)
       ).subscribe((liked:boolean )=> { 
         this.like = liked
-        console.log('this.like ' + this.like)
       }); 
   }
 
@@ -90,7 +82,6 @@ export class EventShowComponent implements OnInit, OnDestroy, AfterViewInit {
         takeUntil(this.destroy$)
       ).subscribe((favorite:boolean )=> { 
         this.favorite = favorite
-        console.log('this.favorite ' + this.favorite)
       }); 
   }
 
@@ -157,9 +148,6 @@ export class EventShowComponent implements OnInit, OnDestroy, AfterViewInit {
 
     this.userAuth = this.authService.getAuthState()
     
-    // this.favorite = this.event?.favorites_users_exists! 
-    // this.like = this.event?.liked_users_exists!
-    
     this.getEvent()
     this.checkLiked()
     this.checFavorite()
@@ -167,11 +155,18 @@ export class EventShowComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngAfterViewInit(): void {
     register();
+      
+      this.swiper = this.swiperRef?.nativeElement.swiper
+      this.swiper?.update();
+      console.log(this.swiper)
+    
     // this.swiper = this.swiperRef?.nativeElement.swiper
+    
     // setTimeout(() => {
     //   // this.swiperCurrentSlide = this.swiper?.realIndex! + 1
     //   // this.swiperTotalSlids =  this.swiper?.slides.length 
-    //   this.swiper?.autoplay.start()
+    //   // this.swiper?.autoplay.start()
+    //   // console.log(this.swiper);
     //   ,1000
     // }) // Без этого костыля автоплей работает только в первой карточке
 
@@ -179,6 +174,7 @@ export class EventShowComponent implements OnInit, OnDestroy, AfterViewInit {
     //   this.swiperCurrentSlide = this.swiper?.realIndex! + 1
     // });
   }
+
 
   ngOnDestroy() {
       // отписываемся от всех подписок
