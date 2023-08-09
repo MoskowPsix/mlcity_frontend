684e205a6a901bbdbfdae577419a82ea321c07da
MoskowPsix <bolshe.kivi@gmail.com>
2023-08-09 18:14:26 +0500

    add filter not button

---
+++ src/app/components/filters_not_button/filters_not_button.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { FiltersNotButtonComponent } from './filters_not_button';
+
+describe('FiltersNotButtonComponent', () => {
+  let component: FiltersNotButtonComponent;
+  let fixture: ComponentFixture<FiltersNotButtonComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FiltersNotButtonComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FiltersNotButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
