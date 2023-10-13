35c7280c119f3908d25ff4e8242f767a364bd18a
MoskowPsix <bolshe.kivi@gmail.com>
2023-10-13 16:18:40 +0500

    six scroll calendula

--- src/app/components/calendula/calendula.component.ts
+++ src/app/components/calendula/calendula.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild, Renderer2 } from '@angular/core';
 import { FilterService } from 'src/app/services/filter.service';
 
 @Component({
@@ -7,7 +7,7 @@ import { FilterService } from 'src/app/services/filter.service';
   styleUrls: ['./calendula.component.scss'],
 })
 export class CalendulaComponent  implements OnInit {
-  constructor( private filterService: FilterService, ) { }
+  constructor( private filterService: FilterService, private renderer: Renderer2 ) { }
   @Input() scroll!: number 
   @Output() dateOutput = new EventEmitter();
   @Input() date!: {dateStart: string, dateEnd: string}
@@ -29,6 +29,13 @@ export class CalendulaComponent  implements OnInit {
    dateStart: number = 0
    dateEnd: number = 0
 
+  getDateYMD(data: number) {
+    let year = new Date(data).getFullYear()
+    let month = new Date(data).getMonth()
+    let day = new Date(data).getDate()
+    // console.log(new Date(year, month, day).toISOString())
+    return new Date(year, month, day).toISOString()
+  }
   onDateOutput() {
     this.dateOutput.emit(this.date)
   }
@@ -40,14 +47,13 @@ export class CalendulaComponent  implements OnInit {
   }
 
   setDate(date: any) {
-    //console.log(date, this.dateStart)
     if (!this.dateStart) {
 
       this.dateStart = date
       this.dateEnd = date
 
-      this.date.dateStart = new Date(date*100000).toISOString()
-      this.date.dateEnd = new Date(date*100000).toISOString()
+      this.date.dateStart = new Date(date).toISOString()
+      this.date.dateEnd = new Date(date).toISOString()
       // this.filterService.setStartDateTolocalStorage(new Date(date*100000).toISOString())
       // this.filterService.setEndDateTolocalStorage(new Date(date*100000).toISOString())
       // this.filterService.changeFilter.next(true)   
@@ -56,14 +62,14 @@ export class CalendulaComponent  implements OnInit {
       this.dateStart = date
       this.dateEnd = date
 
-      this.date.dateStart = new Date(date*100000).toISOString()
-      this.date.dateEnd = new Date(date*100000).toISOString()
+      this.date.dateStart = new Date(date).toISOString()
+      this.date.dateEnd = new Date(date).toISOString()
       // this.filterService.setStartDateTolocalStorage(new Date(date*100000).toISOString())
       // this.filterService.setEndDateTolocalStorage(new Date(date*100000).toISOString())
       // this.filterService.changeFilter.next(true) 
     } else if (this.dateEnd === this.dateStart){
       this.dateEnd = date
-      this.date.dateEnd = new Date(date*100000).toISOString()
+      this.date.dateEnd = new Date(date).toISOString()
       // this.filterService.setEndDateTolocalStorage(new Date(date*100000).toISOString())
       // this.filterService.changeFilter.next(true) 
     } else {
@@ -71,11 +77,21 @@ export class CalendulaComponent  implements OnInit {
       this.dateStart = date
       this.dateEnd = date
 
-      this.date.dateStart = new Date(date*100000).toISOString()
-      this.date.dateEnd = new Date(date*100000).toISOString()
+      this.date.dateStart = new Date(date).toISOString()
+      this.date.dateEnd = new Date(date).toISOString()
       // this.filterService.setStartDateTolocalStorage(new Date(date*100000).toISOString())
       // this.filterService.setEndDateTolocalStorage(new Date(date*100000).toISOString())
       // this.filterService.changeFilter.next(true) 
+    }
+
+    var elem = this.widgetsContent.nativeElement!.offsetWidth
+    let left = document.getElementById(date)!.offsetLeft
+    let right = elem - document.getElementById(date)!.offsetLeft
+
+    if (left > right) {
+      this.widgetsContent.nativeElement.scrollTo({left: left - elem/2, behavior: 'smooth'})
+    } else if (right > left) {
+      this.widgetsContent.nativeElement.scrollTo({right:right - elem/2, behavior: 'smooth'})
     }
     this.onDateOutput()
   }
@@ -86,68 +102,70 @@ export class CalendulaComponent  implements OnInit {
       this.scroll = 500
     } 
     // Подгрузить дату из стора(как понял)
-    this.dateStart  = Math.ceil(new Date(this.date.dateStart).getTime() / 100000)
-    this.dateEnd = Math.ceil(new Date(this.date.dateEnd).getTime() / 100000) 
+    this.dateStart  = new Date(this.date.dateStart).getTime()
+    this.dateEnd = Math.ceil(new Date(this.date.dateEnd).getTime()) 
 
-    let now_date: any = new Date().getTime()
+    let now_date: any =  new Date(this.getDateYMD(new Date().getTime())).getTime()
     let data: any
     let count: number
     for (count = 0; count <= 90; count++) {
       let now_month: any = new Date(now_date).getMonth()
       switch (now_month) {
         case 0:
-          this.date_full[0].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[0].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('1');
           break;
         case 1:
-          this.date_full[1].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[1].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('2');
           break;
         case 2:
-          this.date_full[2].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[2].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('3');
           break;
         case 3:
-          this.date_full[3].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[3].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('4');
           break;
         case 4:
-          this.date_full[4].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[4].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('5');
           break;
         case 5:
-          this.date_full[5].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[5].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('6');
           break;
         case 6:
-          this.date_full[6].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[6].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('7');
           break;
         case 7:
           //console.log('8');
-          this.date_full[7].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[7].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           break;
         case 8:
-          this.date_full[8].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[8].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('9');
           break;
         case 9:
-          this.date_full[9].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[9].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('10');
           break;
         case 10:
-          this.date_full[10].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[10].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('11');
           break;
         case 11:
-          this.date_full[11].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date / 100000)})
+          this.date_full[11].data.push({number: new Date(now_date).getDate(), day_week: new Date(now_date).getDay(), full_date: new Date(now_date).toString(), min: Math.ceil(now_date)})
           //console.log('12');
           break;
       }
       now_date = now_date + 86400000
       this.onDateOutput()
+      setTimeout(() => {this.setDate(new Date(this.getDateYMD(this.dateStart)).getTime())}, 1000);
     }
   }
+
 
 }
 
