c74637b9c0a7ec912c9070052cb97efdccfc539d
bloodright <i.rezys@mail.ru>
2024-01-23 16:44:33 +0500

    create visual component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -85,6 +85,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { EdditSightComponent } from './views/cabinet/my-sights/edit-sight/eddit-sight.component';
 import { SightGalleryComponent } from './components/sight-gallery/sight-gallery.component';
 import { SettingsMenuComponent } from './components/settings-menu/settings-menu.component';
+import { TypesComponent } from './components/types/types.component';
 
 
 
@@ -145,7 +146,8 @@ registerLocaleData(localeRu, 'ru');
     RegistrationComponent,
     EdditSightComponent,
     SightGalleryComponent,
-    SettingsMenuComponent
+    SettingsMenuComponent,
+    TypesComponent
     
     
   ],
