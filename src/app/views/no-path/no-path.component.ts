51c0e7f8c9f9f8276544812674acb5468779ed11
Weqil <ar5ipooshka@yandex.ru>
2024-02-09 17:18:30 +0300

    fix auth in deeplinks

---
+++ src/app/views/no-path/no-path.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-no-path',
+  templateUrl: './no-path.component.html',
+  styleUrls: ['./no-path.component.scss'],
+})
+export class NoPathComponent  implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    // window.open('/home', '_self');
+    this.router.navigateByUrl('/home').then(() => {
+      window.location.href = 'home';
+    });
+  }
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
   BACKEND_URL: 'https://mlcity.ru',
   BACKEND_PORT: '3443',
   DOMAIN: 'mlcity.ru',
-  vkontakteAuthUrl: `https://mlcity.ru:3443/api/social-auth/vkontakte`,
+  vkontakteAuthUrl: `https://www.mlcity.ru:3443/api/social-auth/vkontakte`,
   vkontakteServiceKey: '7c6d50047c6d50047c6d5004647f7f17fc77c6d7c6d50041fa47869a3cc84a7c0bed429'
   // cityName: 'Заречный',
   // cityRegion: 'Свердловская область',
