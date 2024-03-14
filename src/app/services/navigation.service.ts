d923fd7726638bc9a0e683b4269b30687c7271db
MoskowPsix <bolshe.kivi@gmail.com>
2024-03-14 13:38:04 +0500

    add button sight and event tape in map

--- src/app/services/navigation.service.ts
+++ src/app/services/navigation.service.ts
@@ -13,6 +13,7 @@ export class NavigationService {
 
   public modalSearchCityesOpen: BehaviorSubject<boolean> = new BehaviorSubject(false) //открытие модалки с поиском городов
   public modalSearchEventsOpen: BehaviorSubject<boolean> = new BehaviorSubject(false) //открытие модалки с поиском событий
+  public modalEventRadiusShowOpen: BehaviorSubject<boolean> = new BehaviorSubject(false) //открытие модалки с ивентов или местом в радиусе карты
   public modalEventShowOpen: BehaviorSubject<boolean> = new BehaviorSubject(false) //открытие модалки с ивентов или местом - клик по метке на карте
   public modalFiltersOpen: BehaviorSubject<boolean> = new BehaviorSubject(false) //открытие модалки с фильтрами
   public appFirstLoading: BehaviorSubject<boolean> = new BehaviorSubject(true) // первый запуск приложения
@@ -38,6 +39,7 @@ export class NavigationService {
       this.modalSearchEventsOpen.next(false)
       this.modalEventShowOpen.next(false)
       this.modalFiltersOpen.next(false)
+      this.modalEventRadiusShowOpen.next(false)
       //this.appFirstLoading.next(false)
     })
   }
