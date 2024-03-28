ae9318513afec02de6d267c7e0070fcd6dc4fc9f
MoskowPsix <bolshe.kivi@gmail.com>
2024-03-28 19:21:10 +0500

    add form support

---
+++ src/app/services/support.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  Observable,
+  mergeMap,
+  BehaviorSubject,
+  ReplaySubject,
+  tap,
+} from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../models/user';
+import { ToastService } from './toast.service';
+import { TokenService } from './token.service';
+import { UserService } from './user.service';
+import { MessagesAuth } from '../enums/messages-auth';
+import { PasswordReset } from '../models/password-reset';
+import { QueryBuilderService } from './query-builder.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupportService {
+
+  constructor(
+    private http: HttpClient,
+    ) {
+
+    }
+  sendMailSupport(param: any) {
+    const params = {
+      email: param.email,
+      text: param.text,
+      name: param.name
+    }
+    return this.http.post<any>(
+      `${environment.BACKEND_URL}:${environment.BACKEND_PORT}/api/feedback/user`,params
+    );
+  }
+}
--- src/app/views/contacts/contacts.component.ts
+++ src/app/views/contacts/contacts.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EMPTY, Subject, catchError, filter, of, takeUntil } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastService } from 'src/app/services/toast.service';
+import { environment } from 'src/environments/environment';
+import { MessagesAuth } from 'src/app/enums/messages-auth';
+import { MessagesErrors } from 'src/app/enums/messages-errors';
+import { SupportService } from 'src/app/services/support.service';
+ 
+
 
 @Component({
   selector: 'app-contacts',
@@ -6,5 +16,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent {
-  constructor() {}
+  private readonly destroy$ = new Subject<void>();
+
+  constructor(
+    private supportService:SupportService
+  ) {}
+  contactForm!:FormGroup
+
+  onSubmit(){
+    if(this.contactForm.valid){
+      console.log(this.contactForm.value)
+      this.supportService.sendMailSupport(this.contactForm.value).pipe(
+        takeUntil(this.destroy$),
+        catchError(err => {
+          console.log(err)
+          return of(EMPTY)
+        })
+      ).subscribe(ress =>{
+        console.log(ress)
+      })
+    } 
+    else
+    {
+      console.log('no')
+    }
+    console.log(this.contactForm.value.name)
+  }
+
+  ngOnInit() {
+    this.contactForm = new FormGroup(
+      {
+        email: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(50),
+        ]),
+
+        name: new FormControl('',[
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(50)
+        ]),
+        text: new FormControl('',[
+          Validators.required,
+          Validators.minLength(3),
+        ])
+
+      }
+    )
+  }
 }
