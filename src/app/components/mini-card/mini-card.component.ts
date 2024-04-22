ccd7894ed46f27972444171687090aa9ce4e01ce
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2024-04-22 18:45:41 +0500

    add mini-card component

---
+++ src/app/components/mini-card/mini-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+@Component({
+  selector: 'app-mini-card',
+  templateUrl: './mini-card.component.html',
+  styleUrls: ['./mini-card.component.scss'],
+})
+export class MiniCardComponent  implements OnInit {
+
+  @Input() image!: string
+  @Input() title!: string
+  
+  constructor() { }
+
+  ngOnInit() {}
+
+}
