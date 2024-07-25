c0426f914f23f8f76f92e4e40a08f3987da1befb
MoskowPsix <bolshe.kivi@gmail.com>
2024-07-25 13:44:30 +0500

    add: method determine type property for compare and set proprty

--- src/app/clasess/history_content/history_content.ts
+++ src/app/clasess/history_content/history_content.ts
@@ -4,6 +4,7 @@ import { IFile } from 'src/app/models/file'
 import { IPrice } from 'src/app/models/price'
 import { ISight } from 'src/app/models/sight'
 import { ISightType } from 'src/app/models/sight-type'
+import { isEqual } from 'lodash'
 
 export class HistoryContent {
   public origin: any = {}
@@ -19,7 +20,7 @@ export class HistoryContent {
    * Сравнивает два входных значения и при совпадении возвращает true, напротив же false.
    */
   private isDifferentAttributes(origin: any, edit: any): boolean {
-    if (origin === edit) {
+    if (isEqual(origin, edit)) {
       return true
     }
     return false
@@ -29,7 +30,7 @@ export class HistoryContent {
    *
    * Принимает в себя имя свойства и отдаёт значение этого свойства
    */
-  private getPropertyByName(name: string): any {
+  private getProperty(name: string): any {
     return this[name as keyof HistoryContent]
   }
   /**
@@ -37,45 +38,90 @@ export class HistoryContent {
    *
    * Принимает в себя имя свойства класса и значение, которое нужно присвоить свойству.
    */
-  private setPropertyByName(name: string, value: any): any {
-    return (this[name as keyof HistoryContent] = value)
+  private setProperty(name: string, value: any): void {
+    this[name as keyof HistoryContent] = value
   }
   /**
-   * Присваивает значение свойства объекту, кооторый является свойством класса.
+   * Добавляет значение в массив по имени свойства класса.
+   *
+   * Принимает в себя имя свойства класса и значение, которое нужно добавить в массив.
+   */
+  private setPropertyForArrayPush(name: string, value: any): void {
+    this[name as keyof HistoryContent].push(value)
+  }
+  /**
+   * Присваивает значение свойства объекту, который является свойством класса.
    *
    * Принимает в себя имя свойства класса, ключь объекта свойсьва класса, значение которое нужно устаноить.
    */
-  private setPropertyObjectsByName(
+  private setPropertyObjects(name: string, key: string, value: any): void {
+    this[name as keyof HistoryContent][key] = value
+  }
+  /**
+   * Добавляет значение в массив свойства объекта, который является свойством класса.
+   *
+   * Принимает в себя имя свойства класса, ключь объекта свойсьва класса, значение которое нужно добавить в массив.
+   */
+  private setPropertyObjectsForArrayPush(
     name: string,
     key: string,
     value: any,
   ): void {
-    this[name as keyof HistoryContent][key] = value
+    this[name as keyof HistoryContent][key].push(value)
   }
   /**
    * Получение значения свойства объекта, который является свойством класса.
    *
    * Принимает имя объекста свойства класса и ключь объекта свойства класса.
    */
-  private getPropertyObjectsByName(name: string, key: string): any {
+  private getPropertyObjects(name: string, key: string): any {
     return this[name as keyof HistoryContent][key]
+  }
+  /**
+   * Находит и определяет тип свойства класса для понимания как с ним обращаться, как со строкой или как с массивом.
+   *
+   * Входное значение имя свойства.
+   */
+  compareAndSet(name: string): void {
+    const property: any = this.getProperty(name)
+    typeof property === typeof []
+      ? this.compareAndSetArray(name)
+      : this.compareAndSetString(name)
   }
   /**
    * Сравнивает и устанавливает значение свойству класса по имени.
    *
    * Принимает имя свойства класса, который является ещё и ключом объекта свойств класса edited и origin.
    *
-   * Сравнивает два значения объектов по одному ключу и если есть различия,
+   * Сравнивает два строковых значения объектов по одному ключу и если есть различия,
    * то присваивает значения свойству класса с таким же именем как ключ объекта
    */
-  private compareAndSetName(name: string): void {
+  private compareAndSetString(name: string): void {
     if (
       this.isDifferentAttributes(
-        this.getPropertyObjectsByName('origin', name),
-        this.getPropertyObjectsByName('edited', name),
+        this.getPropertyObjects('origin', name),
+        this.getPropertyObjects('edited', name),
       )
     ) {
-      this.setPropertyByName(name, this.edited.name)
+      this.setProperty(name, this.edited.name)
     }
+  }
+  /**
+   * Сравнивает и добавляет значение в массив свойству класса по имени.
+   *
+   * Принимает имя свойства класса, который является ещё и ключом объекта свойств класса edited и origin.
+   *
+   * Сравнивает два массива объектов и если есть различия,
+   * то добавляет в массив значения свойству класса.
+   */
+  private compareAndSetArray(name: string): void {
+    const origin: any[] = this.getPropertyObjects('origin', name)
+    const edited: any[] = this.getPropertyObjects('edited', name)
+    edited.forEach((edit) => {
+      const orig = origin.find((o: any) => o.id === edit.id)
+      if (!isEqual(edit, orig)) {
+        this.setPropertyForArrayPush(name, edit)
+      }
+    })
   }
 }
