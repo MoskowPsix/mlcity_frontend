0fa01b0b9a7b08e6158d489e7a0b4c9c8ba74f94
Weqil <ar5ipooshka@yandex.ru>
2024-07-29 17:08:04 +0500

    add slider

--- src/app/components/materials/edit-slider/edit-slider.component.ts
+++ src/app/components/materials/edit-slider/edit-slider.component.ts
@@ -1,32 +1,40 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core'
-
+import { ToastService } from 'src/app/services/toast.service'
 @Component({
   selector: 'app-edit-slider',
   templateUrl: './edit-slider.component.html',
   styleUrls: ['./edit-slider.component.scss'],
 })
 export class EditSliderComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private toastService: ToastService
+  ) {}
   @Input() files: any[] = []
   @ViewChild('mainPhoto') mainPhoto!: any
   @Output() filesEmit: EventEmitter<any> = new EventEmitter<any>()
   previews: any[] = []
   deleteFiles: any[] = []
 
-  fileChanged(event: any) {
+  fileChanged(event: any, inputBlock: any) {
+    console.log(event)
     // Преобразуем FileList в обычный массив
     let filesArray: File[] = Array.from(event.target.files)
     filesArray.forEach((file: File) => {
       const reader = new FileReader()
       reader.onload = (e: any) => {
-        this.previews.push({
-          link: e.target.result,
-          name: file.name,
-        })
-        this.files.push(file)
+        if (this.files.map((e) => e.name).indexOf(file.name) == -1) {
+          this.previews.push({
+            link: e.target.result,
+            name: file.name,
+          })
+          this.files.push(file)
+        } else {
+          this.toastService.showToast('Файл уже загружен!', 'warning')
+        }
       }
       reader.readAsDataURL(file)
     })
+    event.target.value = null
     this.filesEmit.emit(this.files)
   }
 
