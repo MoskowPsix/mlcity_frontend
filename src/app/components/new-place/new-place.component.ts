13a1ae0fe52e97d16c83e95141aaa6757856e70b
Weqil <ar5ipooshka@yandex.ru>
2024-07-31 18:04:18 +0500

    add new seances

--- src/app/components/new-place/new-place.component.ts
+++ src/app/components/new-place/new-place.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { IPlace } from 'src/app/models/place'
 
 @Component({
   selector: 'app-new-place',
@@ -9,7 +10,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms'
 export class NewPlaceComponent implements OnInit {
   constructor() {}
   placeForm!: FormGroup
+  @Output() seanceEmit = new EventEmitter()
+  @Input() place!: any
+  @Input() placeId!: any
+  @Output() seanceDeleteEmit = new EventEmitter()
+  seancesArray: any[] = []
+  addSeance() {
+    this.seanceEmit.emit(this.placeId)
+  }
+  seanceDelete(event: any) {
+    console.log(event)
+  }
   ngOnInit() {
+    this.seancesArray = this.place.seances
     this.placeForm = new FormGroup({
       address: new FormControl('', [Validators.required]),
     })
