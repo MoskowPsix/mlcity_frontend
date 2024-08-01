beab2b77827d9aadadbe7c82f612b50631c0b15c
Ada-lave <mega.kefi36@gmail.com>
2024-08-01 14:47:30 +0300

    fix version check error(no implement on web)

--- src/app/services/mobile-notification.service.ts
+++ src/app/services/mobile-notification.service.ts
@@ -1,26 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { Capacitor } from '@capacitor/core'
 import { LocalNotifications } from '@capacitor/local-notifications'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MobileNotificationService {
-
   constructor() {
-    LocalNotifications.registerActionTypes({
-      types: [
-        {
-          id: 'UPDATE_APP',
-          actions: [
-            {
-              id: 'UPDATE_APP_ACCEPT',
-              title: 'Обновить приложение',
-            },
-          ],
-        },
-      ],
-    })
-   }
+    if (Capacitor.getPlatform() == 'android' || Capacitor.getPlatform() == 'ios') {
+      LocalNotifications.registerActionTypes({
+        types: [
+          {
+            id: 'UPDATE_APP',
+            actions: [
+              {
+                id: 'UPDATE_APP_ACCEPT',
+                title: 'Обновить приложение',
+              },
+            ],
+          },
+        ],
+      })
+    }
+  }
 
   checkPermission() {
     LocalNotifications.requestPermissions()
