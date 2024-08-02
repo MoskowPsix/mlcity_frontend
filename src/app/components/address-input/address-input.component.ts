c1220c581313d32d88082003ece25201867f2a19
Weqil <ar5ipooshka@yandex.ru>
2024-08-02 09:19:04 +0500

    add address component

--- src/app/components/address-input/address-input.component.ts
+++ src/app/components/address-input/address-input.component.ts
@@ -24,16 +24,29 @@ export class AddressInputComponent implements OnInit {
   setCoords() {
     if (this.place.latitude) {
       this.coords = [this.place.latitude, this.place.longitude]
-      this.addPlacemark([this.place.latitude, this.place.longitude])
+    
     } else {
       this.coords = [55.751574, 37.573856] // Москва по умолчанию
     }
   }
-  onMapReady(event: any) {
+  onMapReady(event: YaReadyEvent<ymaps.Map>) {
     this.map = event
+    this.addPlacemark(this.coords)
+    const search = new ymaps.SuggestView('search-map-')
+    search.events.add('select', () => {})
+
     console.log(this.map)
   }
-  addPlacemark(coords: number[]) {}
+  addPlacemark(coords: number[]) {
+    this.placemark = new ymaps.Placemark(coords)
+    this.map.target.geoObjects.add(this.placemark)
+  }
+  onMapClick(e:YaEvent<ymaps.Map>){
+   const { target, event } = e
+   this.map.target.geoObjects.removeAll()
+   this.addPlacemark([event.get('coords')[0], event.get('coords')[1]])
+   
+  }
   ngOnInit() {
     this.setCoords()
     console.log(this.place)
