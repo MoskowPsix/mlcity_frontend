2c2f892c9668d9e93364e96cf3f7b972854a51e1
Ada-lave <mega.kefi36@gmail.com>
2024-08-02 10:40:37 +0300

    add circle buttons to range selection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -124,6 +124,7 @@ import { SwitchTypeService } from './services/switch-type.service'
 import { TestIframeComponent } from './views/test-iframe/test-iframe.component'
 import { CircleButtonComponent } from './components/materials/circle-button/circle-button.component'
 import { RoundedButtonComponent } from './components/materials/rounded-button/rounded-button.component'
+import { RangeSelectionButtonComponent } from './components/materials/range-selection-button/range-selection-button.component'
 const mapConfig: YaConfig = {
   apikey: environment.apiKeyYandex + '&' + `suggest_apikey=${environment.apiKeyYandexSubject}`,
   lang: 'ru_RU',
@@ -215,8 +216,9 @@ registerLocaleData(localeRu, 'ru')
     UpdateVersionModalComponent,
     ModalCheckEmailComponent,
     FooterItemComponent,
-    TestPageComponent,
     TypeSwitherComponent,
+    TestPageComponent,
+    RangeSelectionButtonComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/materials/range-selection-button/range-selection-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-range-selection-button',
+  templateUrl: './range-selection-button.component.html',
+  styleUrls: ['./range-selection-button.component.scss'],
+})
+export class RangeSelectionButtonComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
