7a80746001e901f0725f08f320ea451f2e620ca9
Ada-lave <mega.kefi36@gmail.com>
2024-08-02 12:58:11 +0300

    add action on click

--- src/app/components/materials/range-selection-button/range-selection-button.component.ts
+++ src/app/components/materials/range-selection-button/range-selection-button.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-range-selection-button',
   templateUrl: './range-selection-button.component.html',
   styleUrls: ['./range-selection-button.component.scss'],
 })
-export class RangeSelectionButtonComponent  implements OnInit {
-
+export class RangeSelectionButtonComponent implements OnInit {
+  state: boolean = true
   constructor() { }
 
-  ngOnInit() {}
-
+  changeState() {
+    this.state = !this.state
+    console.log('share')
+  }
+  ngOnInit() { }
 }
