10c0e934686cfccb5ad9fd47ec098d8e5d54b24a
Weqil <ar5ipooshka@yandex.ru>
2024-08-16 16:05:25 +0500

    add slider video

--- src/app/components/materials/show-slider/show-slider.component.ts
+++ src/app/components/materials/show-slider/show-slider.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, inject, Input, OnInit } from '@angular/core'
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { IonicSlides } from '@ionic/angular'
 import { register } from 'swiper/element/bundle'
 import { environment } from 'src/environments/environment'
+import { DomSanitizer } from '@angular/platform-browser'
+
 register()
 @Component({
   selector: 'app-show-slider',
@@ -11,13 +13,19 @@ register()
 })
 export class ShowSliderComponent implements OnInit {
   constructor() {}
-  @Input() files?: File[]
+  @Input() files?: any[]
   swiperModules = [IonicSlides]
+  sanitizer: DomSanitizer = inject(DomSanitizer)
 
   checkUrl(file: any) {
+    console.log(file)
     return file.link.includes('https') || file.link.includes('http')
       ? file.link
       : environment.BACKEND_URL + ':' + environment.BACKEND_PORT + file.link
   }
+  iframeUrl(url: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url)
+  }
+
   ngOnInit() {}
 }
