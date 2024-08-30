274c08406684e1ce1dadf5144297854c5b464354
Ada-lave <mega.kefi36@gmail.com>
2024-08-30 07:57:48 +0300

    redirect user after save event changes

--- src/app/views/cabinet/edit/edit-event/edit-event.component.ts
+++ src/app/views/cabinet/edit/edit-event/edit-event.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { catchError, EMPTY, of, Subject, takeUntil, tap } from 'rxjs'
 import { IEvent } from 'src/app/models/event'
 import { EventsService } from 'src/app/services/events.service'
@@ -43,6 +43,7 @@ export class EditEventComponent implements OnInit {
     private eventsService: EventsService,
     private loadingService: LoadingService,
     private editService: EditService,
+    private router: Router,
   ) {}
   private readonly destroy$ = new Subject<void>()
   event!: IEvent
@@ -303,9 +304,9 @@ export class EditEventComponent implements OnInit {
         this.getPlaces()
         this.loadingService.hideLoading()
         this.editForm.patchValue({
-          name: res.event.name,
-          sponsor: res.event.sponsor,
-          description: res.event.description,
+          name: res.name,
+          sponsor: res.sponsor,
+          description: res.description,
         })
         if (priceArray) {
           priceArray.forEach((price: any) => {
@@ -499,6 +500,7 @@ export class EditEventComponent implements OnInit {
           this.loadingService.hideLoading()
           if (res.status == 'success') {
             this.toastService.showToast('Событие отправленно на проверку', 'success')
+            this.router.navigate(['/cabinet/events'])
           }
         })
     }
