962dcf500c2ae3af61bd0bf5606de8f282a55d4f
Weqil <ar5ipooshka@yandex.ru>
2024-09-12 14:14:50 +0500

    fix footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,8 +64,8 @@ export class AppComponent implements OnInit {
       if (this.url.includes('/cabinet/sights/edit')) {
         this.url = '/cabinet/sights/edit'
       }
-      if (this.url.includes('/cabinet/event/edit')) {
-        this.url = '/cabinet/sights/event'
+      if (this.url.includes('/cabinet/events/edit')) {
+        this.url = '/cabinet/events/edit'
       }
       this.iframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`${environment.BASE_URL}${this.url}`)
     })
