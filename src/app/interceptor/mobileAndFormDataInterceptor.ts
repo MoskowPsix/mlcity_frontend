03e7b2de3a180b128f517bd087ffe66daf991a3a
Weqil <ar5ipooshka@yandex.ru>
2024-12-03 12:18:01 +0500

    test interceptor

--- src/app/interceptor/mobileAndFormDataInterceptor.ts
+++ src/app/interceptor/mobileAndFormDataInterceptor.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core'
+import { inject, Injectable } from '@angular/core'
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { Capacitor } from '@capacitor/core'
-
+import { ToastService } from '../services/toast.service'
 @Injectable()
 export class MobileAndFormDataInterceptor implements HttpInterceptor {
   platform: string = Capacitor.getPlatform()
-
+  toastService: ToastService = inject(ToastService)
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let updatedRequest: any = req
-
+    this.toastService.showToast(this.platform, 'warning')
     if (this.platform == 'android') {
       if (req.method == 'POST' && req.body instanceof FormData) {
         updatedRequest = req.clone({
