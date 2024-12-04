eb57d9596d83c8a3bcfe344f870a6d5417a44f5f
Weqil <ar5ipooshka@yandex.ru>
2024-12-04 19:48:08 +0500

    test 4

--- src/app/interceptor/mobileAndFormDataInterceptor.ts
+++ src/app/interceptor/mobileAndFormDataInterceptor.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs'
 import { Capacitor } from '@capacitor/core'
 import { ToastService } from '../services/toast.service'
 function generateGuid(): string {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+  return 'xxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
       v = c == 'x' ? r : (r & 0x3) | 0x8
     return v.toString(16)
@@ -21,7 +21,7 @@ export class MobileAndFormDataInterceptor implements HttpInterceptor {
       if (req.method == 'POST' && req.body instanceof FormData) {
         updatedRequest = req.clone({
           setHeaders: {
-            'Content-Type': `multipart/form-data;  boundary=----WebKitFormBoundaryxMEzBVV6qZxQarAI`,
+            'Content-Type': `multipart/form-data;  boundary= ${boundary}`,
           },
         })
       }
