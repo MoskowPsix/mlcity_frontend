fa269ba1f79dd9a18944f1d19ab46f29eadfc629
Weqil <ar5ipooshka@yandex.ru>
2024-12-04 19:21:54 +0500

    test v2

--- src/app/interceptor/mobileAndFormDataInterceptor.ts
+++ src/app/interceptor/mobileAndFormDataInterceptor.ts
@@ -3,14 +3,20 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Observable } from 'rxjs'
 import { Capacitor } from '@capacitor/core'
 import { ToastService } from '../services/toast.service'
-import { uuid } from 'uuidv4'
+function generateGuid(): string {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
 @Injectable()
 export class MobileAndFormDataInterceptor implements HttpInterceptor {
   platform: string = Capacitor.getPlatform()
   toastService: ToastService = inject(ToastService)
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let updatedRequest: any = req
-    let boundary = '----WebKitFormBoundary' + uuid()
+    let boundary = '----WebKitFormBoundary' + generateGuid()
     if (this.platform == 'android') {
       if (req.method == 'POST' && req.body instanceof FormData) {
         updatedRequest = req.clone({
