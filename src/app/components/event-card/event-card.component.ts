84d6a2c64e7e3271b13de4ff9fab059b6d6a2f4b
Weqil <ar5ipooshka@yandex.ru>
2024-12-05 13:14:57 +0500

    fix header

--- src/app/components/event-card/event-card.component.ts
+++ src/app/components/event-card/event-card.component.ts
@@ -417,6 +417,9 @@ export class EventCardComponent implements OnInit, OnDestroy, AfterViewInit {
       if (element.pivot.last) {
         if (element.name == Statuses.new) {
           status = { name: Statuses.changed }
+        }
+        if (element.name == 'Изменено') {
+          status = { name: Statuses.changed }
         } else {
           status = element
         }
