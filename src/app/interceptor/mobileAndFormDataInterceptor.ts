0f3164f8138fcb187b3bcdc91acd90815eef5165
Weqil <ar5ipooshka@yandex.ru>
2024-12-05 11:45:47 +0500

    test 10

--- src/app/interceptor/mobileAndFormDataInterceptor.ts
+++ src/app/interceptor/mobileAndFormDataInterceptor.ts
@@ -21,8 +21,7 @@ export class MobileAndFormDataInterceptor implements HttpInterceptor {
       if (req.method == 'POST' && req.body instanceof FormData) {
         updatedRequest = req.clone({
           setHeaders: {
-            'Content-Type': 'multipart/form-data',
-            'enctype': 'multipart/form-data'
+            enctype: 'multipart/form-data',
           },
         })
       }
