2dc38ba9f22ddd4214d6f1732af8c986729c80d6
Weqil <ar5ipooshka@yandex.ru>
2024-12-09 18:29:35 +0500

    add version

--- src/app/views/cabinet/cabinet.component.ts
+++ src/app/views/cabinet/cabinet.component.ts
@@ -19,6 +19,7 @@ export class CabinetComponent implements OnInit {
   ) {}
   user!: IUser
   test: string = 'test'
+  appVersion: any = ''
   platformType: string = Capacitor.getPlatform()
   private readonly destroy$ = new Subject<void>()
   onLogout() {
@@ -29,6 +30,15 @@ export class CabinetComponent implements OnInit {
   }
   ionViewWillEnter() {
     this.user = this.userService.getUserFromLocalStorage()
+    this.platformType = Capacitor.getPlatform()
+    if (this.platformType == 'web') {
+      Device.getInfo().then((device: any) => {
+        console.log(device)
+        if (device.appVersion) {
+          this.appVersion = device.appVersion
+        }
+      })
+    }
   }
 
   ngOnInit() {
